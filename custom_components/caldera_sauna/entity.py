f7fd5c123aa4b3e756091dbcd89dbb3da5f8dd80
Ark <ark@recursive.games>
2026-08-23 02:36:18 -0600

    fix device info import

--- custom_components/caldera_sauna/entity.py
+++ custom_components/caldera_sauna/entity.py
@@ -1,7 +1,7 @@
 """Shared base entity for Caldera Sauna platforms."""
 from __future__ import annotations
 
-from homeassistant.helpers.device_info import DeviceInfo
+from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
@@ -17,7 +17,7 @@ class CalderaEntity(CoordinatorEntity[CalderaCoordinator]):
         super().__init__(coordinator)
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, coordinator.address)},
-            connections={("bluetooth", coordinator.address)},
+            connections={(CONNECTION_BLUETOOTH, coordinator.address)},
             manufacturer="Relaxe / Caldera",
             name=coordinator.sauna.name or "Sauna",
         )
